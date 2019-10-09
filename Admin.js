b2dd326635c6246266951a518eece805bfac3c2c
Benjamin <bela19ac@student.cbs.dk>
2019-10-09 15:28:52 +0200

    test

--- Admin.js
+++ Admin.js
@@ -1,1 +1,15 @@
 /*Skal modtage
+/*
+
+
+ */
+<!DOCTYPE html>
+
+<html>
+    <a href="Confirmation.html"
+<button type="button">Book_a_Table</button>
+    </a>
+<script>
+ var myVariable="Accept"
+
+</html>
