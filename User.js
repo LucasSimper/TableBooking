8aa4c1065ddb75df6b9766fae86d77a5d67da765
Reuther <jere19ac@student.cbs.dk>
2019-10-15 10:12:18 +0200

    created User class

--- User.js
+++ User.js
@@ -3,3 +3,9 @@
 /* skal kunne logge ind- ud fra User Data
 skal den kunne vide hvilken bruger der logger ind
  */
+class Users {
+    constructor(firstName, lastName, password, age, gender, eMail, phoneNumber, birthday){
+        this firstName = firstName
+        this lastName = lastName
+    }
+}
