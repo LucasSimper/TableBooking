3ba8d8abf56cc43ba8e119ad003c5bb6d95f83a5
Reuther <jere19ac@student.cbs.dk>
2019-10-15 10:20:46 +0200

    added comments

--- User.js
+++ User.js
@@ -7,5 +7,17 @@ class Users {
     constructor(firstName, lastName, password, age, gender, eMail, phoneNumber, birthday){
         this firstName = firstName
         this lastName = lastName
+
     }
 }
+
+//array is empty. every new user will be pushed to the array
+var allUsers = []
+
+//use allUsers.push() to add new Users to the array
+new Users()
+
+// check if local storage for Users is empty if localStorage.getItem("Users") == null then push new Users to allUser array
+// if there are Users in the local Storage, retrieve the local storage allUsers = JSON.parse(localStorage.getItem("Users"). and then we push to allUsers
+
+// push allUsers array to localstorage
