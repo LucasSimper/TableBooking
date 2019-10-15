051173a7a105bbb29e5fdd453b869a220f83cf1a
Reuther <jere19ac@student.cbs.dk>
2019-10-15 17:22:36 +0200

    User changes

--- User.js
+++ User.js
@@ -3,21 +3,43 @@
 /* skal kunne logge ind- ud fra User Data
 skal den kunne vide hvilken bruger der logger ind
  */
-class Users {
-    constructor(firstName, lastName, password, age, gender, eMail, phoneNumber, birthday){
-        this firstName = firstName
-        this lastName = lastName
-
+class User {
+    constructor(firstName, lastName, password, age, gender, eMail, phoneNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.age = age;
+        this.gender = gender;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
     }
+    var allUsers = [];
+
+    function storeUsers () {
+        user_id_generator();
+
+        var userObj = new User (document.getElementById("firstName").value, document.getElementById("lastName").value, document.getElementById("password").value,
+            document.getElementById("age").value, document.getElementById("gender").value, document.getElementById("eMail").value, document.getElementById("phoneNumber").value)
+
+        userObj.user_id = user_idgen;
+        console.log(userObj);
+
+        localStorage.setItem("allUsers", JSON.stringify(allUsers));
+        console.log(JSON.parse(localStorage.getItem("allUsers")));
+        alert("Det er nu blevet gemt!")
+    }
+
 }
 
 //array is empty. every new user will be pushed to the array
-var allUsers = []
 
 //use allUsers.push() to add new Users to the array
-new Users()
+/*new Users(){
+    this.firstName = localStorage.getItem(firstName);
+    this.lastName =localStorage.getItem(lastName);
+}
 
 // check if local storage for Users is empty if localStorage.getItem("Users") == null then push new Users to allUser array
 // if there are Users in the local Storage, retrieve the local storage allUsers = JSON.parse(localStorage.getItem("Users"). and then we push to allUsers
 
-// push allUsers array to localstorage
+// push allUsers array to localstorage */
