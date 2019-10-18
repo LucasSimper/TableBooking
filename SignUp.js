b9d89ec123f82b427ceb1de6929003c200a6aa2c
Reuther <jere19ac@student.cbs.dk>
2019-10-18 09:09:10 +0200

    reuther

--- SignUp.js
+++ SignUp.js
@@ -11,26 +11,37 @@ var genderMale = document.getElementById("genderRadioMale")
 // bruges ikke lige nu: var signUpButton = document.getElementById("signUpButton")
 
 
-/*
-Funktion til at omregne fødselsdato til en reel alder- virker ikke (Muligvis fordi vi skal ændre age-objektet fra localstorage til et objekt??)
+
+//Funktion til at omregne fødselsdato til en reel alder
 function calculateAge () {
-    var b = Date.now();
-    var a = age.getMilliseconds();
-    var ageInMs = b-a;
-    var ageInYears = ageInMs/(24*3600*365.25*1000);
-    Math.floor (ageInYears);
+    var todayInMs = Date.now();
+    var birthDate = new Date(age.value);
+    var birthMS = birthDate.getTime();
+    var ageInMs = todayInMs-birthMS;
+    ageInYears = Math.floor(ageInMs/(24*3600*365.25*1000));
    return ageInYears;
 }
-*/
+function controlGender() {
+    gender =""
+        if (genderMale.checked == true){
+            gender = "Male";
+        }
+        else {
+            gender = "Female";
+        }
+        return (gender);
+
+    }
+
 
 
 // Funktion til at kontrollere at alle inputs bliver overholdt
 function checkInputs() {
     var passed = true;
-    // Variabel til at vise at det KUN må indeholde digits
+    // Variabel til at vise at der KUN må indeholde digits- bruges i telefonnummer
     var numbers = /^\d+$/;
 
-    if (phoneNumber.value.match(numbers) || phoneNumber.value<1) {
+   /* if (phoneNumber.value.match(numbers) || phoneNumber.value<1) {
         passed = true
     }
     else {
@@ -43,7 +54,6 @@ function checkInputs() {
         document.getElementById("firstName").style.borderColor = "red";
         passed = false
     }
-
     else if (lastName.value == "") {
         alert("You need to fill out all the boxes");
         document.getElementById("lastName").style.borderColor = "red";
@@ -86,34 +96,29 @@ function checkInputs() {
         document.getElementById("password" && "confirmPassword").style.borderColor = "red";
         passed = false;
     }
-    if (password.value.length>0 && password.value.length<8){
+    if (password.value.length>0 && password.value.length<8) {
         alert("Your password must be atleat 8 characters");
         document.getElementById("password").style.borderColor = "red";
         passed = false;
     }
+    if (calculateAge()<18){
+        alert("You need to be atleast 18 years to create an account")
+        passed = false;
+    }*/
+    //if( der er tal i lastname OR first name){
+        //alert("Der må ikke være tal bla bla)
+        // passed = false;
     //Create function that makes sure that day of birth cannot be set to anything else than an actual date.
     // Create function that makes sure that name cannot consist of anything else than letters
 
-    return passed
+    return passed;
 }
 // Denne funktion skal skiftes til at være en onclick funktion. SKal bruge var signUpButton men ved ikke hvordan
-    function storeLogin() {
+   /* function Sign_Up() {
         if (checkInputs () == true) {
-            // virker ikke lige nu storeUsers();
-             localStorage.setItem ("firstName", firstName.value);
-            localStorage.setItem("lastName", lastName.value);
-            localStorage.setItem("password", password.value);
-            if ( genderMale.checked == true){
-                localStorage.setItem("gender", "Male");
-            }
-            else {
-                localStorage.setItem("gender", "Female");
-            }
-            localStorage.setItem("phoneNumber", phoneNumber.value);
-            localStorage.setItem("eMail", eMail.value);
-            //localStorage.setItem("age", age.value);
+            storeUsers();
         }
         alert ("You have now created a user! We will now direct you to the booking system!");
 
-    }
+    }*/
 
--- User.js
+++ User.js
@@ -1,10 +1,10 @@
 //Skal oprettes 2 Strings- username og password
 
-/* skal kunne logge ind- ud fra User Data
-skal den kunne vide hvilken bruger der logger ind
- */
-/*class User {
-    constructor(firstName, lastName, password, age, gender, eMail, phoneNumber){
+/*skal kunne logge ind- ud fra User Data
+skal den kunne vide hvilken bruger der logger ind*/
+
+class User {
+    constructor(firstName, lastName, password, age, gender, eMail, phoneNumber, id ){
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
@@ -12,36 +12,53 @@ skal den kunne vide hvilken bruger der logger ind
         this.gender = gender;
         this.eMail = eMail;
         this.phoneNumber = phoneNumber;
+        this.id = id;
+    }}
+//The empty array, that the users will get stored into
+var allUsers = [];
+//Variable to get the id_gen function to work.
+var id_generator = 0;
+
+    function id_gen () {
+        id_generator ++;
+        return id_generator;
     }
-    var allUsers = [];
+    //Function to control which radiobutton is checked
+
+
+
 
     function storeUsers () {
-       // Make a function that creates a user_id_generator();
+        if (checkInputs()== true){
+        id_gen();
 
-        var userObj = new User (document.getElementById("firstName").value, document.getElementById("lastName").value, document.getElementById("password").value,
-            document.getElementById("age").value, document.getElementById("gender").value, document.getElementById("eMail").value, document.getElementById("phoneNumber").value)
-
-        //Adds the value of the user_ID
-        userObj.User_id = user_idgen;
+        var userObj = new User(
+            document.getElementById("firstName").value,
+            document.getElementById("lastName").value,
+            document.getElementById("password").value,
+            calculateAge(),
+            controlGender(),
+            document.getElementById("eMail").value,
+            document.getElementById("phoneNumber").value);
+        userObj.id = id_generator;
         console.log(userObj);
 
-       Pushes the array with user-objects into our localstorage
-        localStorage.setItem("allUsers", JSON.stringify(allUsers));
-        console.log(JSON.parse(localStorage.getItem("allUsers")));
-        alert("Det er nu blevet gemt!")
-    }
+        allUsers.push(userObj);
 
-}
+        console.log(allUsers);
+
+      // Pushes the array with user-objects into our localstorage
+        localStorage.setItem("allUsers", JSON.stringify(allUsers));
+            console.log(JSON.parse(localStorage.getItem("allUsers")));
+         alert("Thank you for creating a user! We will now direct you to the booking system!");
+         //   document.location = "GUI.html";
+    }
+    }
 
 //array is empty. every new user will be pushed to the array
 
 //use allUsers.push() to add new Users to the array
-new Users(){
-    this.firstName = localStorage.getItem(firstName);
-    this.lastName =localStorage.getItem(lastName);
-}
 
-// check if local storage for Users is empty if localStorage.getItem("Users") == null then push new Users to allUser array
 // if there are Users in the local Storage, retrieve the local storage allUsers = JSON.parse(localStorage.getItem("Users"). and then we push to allUsers
 
-// push allUsers array to localstorage */
+// push allUsers array to localstorage
