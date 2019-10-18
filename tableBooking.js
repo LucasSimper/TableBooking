5706a4031b8744cd3bac213c1cf8197b97ebf84c
Benjamin <bela19ac@student.cbs.dk>
2019-10-18 09:28:11 +0200

    first commit

---
+++ tableBooking.js
@@ -0,0 +1,19 @@
+
+function userRequest(){
+    alert("Request accepted!");
+}
+
+function adminConfirm(){
+    document.getElementById("userConfirmation").innerHTML = "Request accepted!";
+}
+
+function userRequest(){
+    alert("Request denied");
+
+}
+
+
+function  adminConfirm(){
+    document.getElementById(userConfirmation).innerHTML = "Request denied!";
+
+}
