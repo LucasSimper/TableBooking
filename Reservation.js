5ee54da59947aed13fa5be35a0a60f8e8ffabd77
Reuther <jere19ac@student.cbs.dk>
2019-10-23 08:48:44 +0200

    Updating existing files before Hackathon

--- Reservation.js
+++ Reservation.js
@@ -3,3 +3,40 @@
 /*Skal derigennem også reservere bordet. Selvom bordet ikke er blevet bekræftet endnu
 skal andre brugere ikke kunne booke bordet.
  */
+// lav et array med 3 variable- 3 forskellige datoer og alle med false statement
+class reservations {
+    constructor (userID, date, status) {
+        var user_id = "";
+        var date = "";
+        var status = "";
+    }}
+
+
+var bookTableButton = document.getElementById("book_table")
+
+var reservation1 = new reservations(
+    1, 2019-10-19, false);
+var reservation2 = new reservations(
+    2,2019-10-08, false);
+var reservation3 = new  reservations(
+    3, 2019-10-21,true);
+var storedReservations = [reservation1, reservation2, reservation3];
+
+
+document.createElement("")
+//if statement hvis noget er korrekt- createElement funktion
+
+
+
+    // Funktion til at filtrere i den data-
+   // og se efter om datoen er overskredet nuværende dato
+    // Derefter se om det er en true eller false statement
+    //Hvis statement er false, vis i en table, Hvis true, vis i en anden table
+    //
+   function storereservations() {
+
+   }
+
+   //Dummy data- 3 forskellige
+
+//Skal kunne finde status på dem
